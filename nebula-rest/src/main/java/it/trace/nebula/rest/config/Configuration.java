2656bba4a2601673696b86bf23af1d367538dbd2
auhigji <auhigji@gmail.com>
2011-06-04 11:03:34 +0800

    rest and rest-sample

---
+++ nebula-rest/src/main/java/it/trace/nebula/rest/config/Configuration.java
@@ -0,0 +1,13 @@
+package it.trace.nebula.rest.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Configuration {
+
+    // Consumes
+    // Produce
+    // getPathSegments
+    private final Map<Object, Object> namespaces = new HashMap<Object, Object>();
+
+}
---
+++ nebula-rest/src/main/java/it/trace/nebula/rest/config/Resource.java
@@ -0,0 +1,16 @@
+package it.trace.nebula.rest.config;
+
+
+public class Resource {
+
+    private final String path;
+
+    public Resource(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
