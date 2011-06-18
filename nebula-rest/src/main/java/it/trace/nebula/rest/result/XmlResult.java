9bedce02b85780b8e0d782564147e5c44d94401d
auhigji <auhigji@gmail.com>
2011-06-18 11:50:16 +0800

    delete json-lib

--- nebula-rest/src/main/java/it/trace/nebula/rest/result/XmlResult.java
+++ nebula-rest/src/main/java/it/trace/nebula/rest/result/XmlResult.java
@@ -3,17 +3,14 @@ package it.trace.nebula.rest.result;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONObject;
-import net.sf.json.xml.XMLSerializer;
-
 public class XmlResult implements Result {
 
     @Override
     public void onSuccess(HttpServletRequest request, HttpServletResponse response, Object data) {
-        JSONObject jo = JSONObject.fromObject(data);
-        XMLSerializer s = new XMLSerializer();
-        String xml = s.write(jo);
-        System.out.println(xml);
+        //        JSONObject jo = JSONObject.fromObject(data);
+        //        XMLSerializer s = new XMLSerializer();
+        //        String xml = s.write(jo);
+        //        System.out.println(xml);
     }
 
     @Override
