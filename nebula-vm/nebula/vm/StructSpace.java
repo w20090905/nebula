17655ad76f9545454a0a0e41c3366eae3fb112ec
wangshilian <wangshilian@gmail.com>
2012-02-01 14:56:06 +0800

    new full redesign

---
+++ nebula-vm/nebula/vm/StructSpace.java
@@ -0,0 +1,16 @@
+package nebula.vm;
+/***
+ * Excerpted from "Language Implementation Patterns",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
+***/
+import java.util.Arrays;
+
+public class StructSpace {
+	int[] fields;
+    public StructSpace(int nfields) { this.fields = new int[nfields]; }
+    public String toString() { return Arrays.toString(fields); }
+}
