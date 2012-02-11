b5038e370f2af1a00855807e54cbf71e0311ee2c
wangshilian <wangshilian@ivpc209.ivisionsh.com>
2012-02-11 11:36:01 +0800

    de

--- nebula-vm/src/Test.java
+++ nebula-vm/src/Test.java
@@ -4,11 +4,12 @@ public class Test {
 
 		int j=0;
 		long start = System.nanoTime();	
-		for(int k=0;k<100;k++){
+		int max = 1000;
+		for(int k=0;k<max;k++){
 		for(int i =0;i<10000;i++){
 			j++;
 		}}
 		long end = System.nanoTime();
-		System.out.println(Test.class.getName() + " : " + (end - start)/100 +"   " + j);
+		System.out.println(Test.class.getName() + " : " + (end - start)/max +"   " + j);
 	}
 }
