f2bed1428e3eb4307122bce9a134ce929b3da01d
wangshilian <wangshilian@gmail.com>
2012-05-24 19:48:42 +0800

    add cascade get parent attr

--- nebula-compiler/src/main/java/nebula/compiler/Type.java
+++ nebula-compiler/src/main/java/nebula/compiler/Type.java
@@ -1,115 +1,114 @@
 package nebula.compiler;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
+import java.util.Properties;
 
 public class Type {
 
-    public static final String Master = "Master";
-    public static final String Attribute = "Attribute";
-    public static final String Underlying = "Underlying";
-    public static final String Sequence = "Sequence";
-    public static final String Scala = "Scala";
-    public static final String Eembedded = "Eembedded";
+	public static final String Master = "Master";
+	public static final String Attribute = "Attribute";
+	public static final String Underlying = "Underlying";
+	public static final String Sequence = "Sequence";
+	public static final String Scala = "Scala";
+	public static final String Eembedded = "Eembedded";
 
-    String name;
-    Alias nameAlias;
-    String displayName;
+	String name;
+	Alias nameAlias;
+	String displayName;
 
-    boolean standalone = true;
+	boolean standalone = true;
 
-    String master;
+	String master;
 
-    Type declaringType = null;
+	Type declaringType = null;
 
-    List<Field> fields;
+	List<Field> fields;
 
-    Map<String,Object> attrs = new HashMap<>();
-    // Type declaringType;
+	Properties attrs;
+	// Type declaringType;
 
-    final Type residedType;    
-    final Type superType;    
-    final String rawType;
+	final Type residedType;
+	final Type superType;
+	final String rawType;
 
-    static Type BUILDERINTYPE = new Type("BuildInType");
-    static Type ENTITY = new Type("ENTITY");
-    
-    private Type(String name) {
-        this(name,null,null);
-    }
-    
-    Type(String name,Type superType) {
-        this(name,superType,superType.rawType);
-    }
-    
-    Type(String name,Type superType,String rawType) {
-        super();
-        this.residedType = null;
-        this.superType = superType;
-        this.rawType = rawType;
-        
-        this.name = name;
-        this.fields = new ArrayList<Field>();
-    }
+	static Type BUILDERINTYPE = new Type("BuildInType");
+	static Type ENTITY = new Type("ENTITY");
 
-    Type(Type residedType,String name,Type superType) {
-    	this(residedType, name, superType, superType.rawType);
-    }
-    
-    Type(Type residedType,String name,Type superType,String rawType) {
-        super();
-        this.residedType = residedType;
-        this.superType = superType;
-        this.rawType = rawType;
-        
-        standalone = false;
-        
-        this.name = name;
-        this.fields = new ArrayList<Field>();
-    }
-    
-    public String getName() {
-        return name;
-    }
+	private Type(String name) {
+		this(name, null, null);
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	Type(String name, Type superType) {
+		this(name, superType, superType.rawType);
+	}
 
-    public String getMaster() {
-        return master;
-    }
+	Type(String name, Type superType, String rawType) {
+		this(null, name, superType, rawType);
+	}
 
-    public void setMaster(String master) {
-        this.master = master;
-    }
+	Type(Type residedType, String name, Type superType) {
+		this(residedType, name, superType, superType.rawType);
+	}
 
-    public List<Field> getFields() {
-        return fields;
-    }
+	Type(Type residedType, String name, Type superType, String rawType) {
+		super();
+		this.residedType = residedType;
+		this.superType = superType;
+		this.rawType = rawType;
 
-    public String getDisplayName() {
-        return this.displayName;
-    }
+		if (residedType != null) {
+			standalone = false;
+		}
 
-    public boolean isStandalone() {
-        return standalone;
-    }
+		if (this.superType != null) {
+			attrs = new Properties(this.superType.attrs);
+		} else {
+			attrs = new Properties();
+		}
+		this.name = name;
+		this.fields = new ArrayList<Field>();
+	}
 
-    public void setStandalone(boolean standalone) {
-        this.standalone = standalone;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Type getDeclaringType() {
-        return declaringType;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setDeclaringType(Type declaringType) {
-        this.declaringType = declaringType;
-    }
+	public String getMaster() {
+		return master;
+	}
+
+	public void setMaster(String master) {
+		this.master = master;
+	}
+
+	public List<Field> getFields() {
+		return fields;
+	}
+
+	public String getDisplayName() {
+		return this.displayName;
+	}
+
+	public boolean isStandalone() {
+		return standalone;
+	}
+
+	public void setStandalone(boolean standalone) {
+		this.standalone = standalone;
+	}
+
+	public Type getDeclaringType() {
+		return declaringType;
+	}
+
+	public void setDeclaringType(Type declaringType) {
+		this.declaringType = declaringType;
+	}
 
 	@Override
 	public String toString() {
@@ -117,5 +116,4 @@ public class Type {
 				+ master + ", declaringType=" + declaringType + ", fields=" + fields + "]";
 	}
 
-   
 }
