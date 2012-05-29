0ac448afbcf52bbec55846db5d2ee1487bfec06b
wangshilian <wangshilian@gmail.com>
2012-05-29 17:32:09 +0800

    d

---
+++ nebula-engine/src/test/java/http/engine/TypeEditResouceTest.java
@@ -0,0 +1,15 @@
+package http.engine;
+
+import junit.framework.TestCase;
+
+public class TypeEditResouceTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
