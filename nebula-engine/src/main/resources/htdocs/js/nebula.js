e459f624b003a42324a0d34f0b4954a8ee85be31
auhigji <auhigji@gmail.com>
2012-09-24 20:10:28 +0800

    neView? No! ngView.

--- nebula-engine/src/main/resources/htdocs/js/nebula.js
+++ nebula-engine/src/main/resources/htdocs/js/nebula.js
@@ -2,8 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('nebula', ['nebula.filters', 'nebula.services', 'nebula.directives','ngResource']).
-  config(['$routeProvider', function($routeProvider) {
+var nebulaModule = angular.module('nebula', ['nebula.filters', 'nebula.services', 'nebula.directives','ngResource']);
+
+nebulaModule.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/t/angularjs/:typename-:cat.html', {template: 'angularjs/angularjs-edit.html', controller: AngularJSCtrl});
 	$routeProvider.when('/t/template/:typename-:cat.html.ftl', {template: 'angularjs/angularjs-edit.html', controller: FreeMarkerCtrl});
 	$routeProvider.when('/d/:typename', {template: 'angularjs/{{typename}}-list.html',   controller: EntityListCtrl});
@@ -12,4 +13,35 @@ angular.module('nebula', ['nebula.filters', 'nebula.services', 'nebula.directive
     $routeProvider.when('/view1', {template: 'angularjs/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {template: 'angularjs/partial2.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+}]);
+
+nebulaModule.directive('name', function() {
+	return {
+		require : 'ngModel',
+		link : function(scope, elm, attrs, ctrl) {
+			ctrl.$parsers.unshift(function(viewValue) {
+				if (viewValue) {
+					ctrl.$setValidity('name', true);
+					return viewValue;
+				} else {
+					ctrl.$setValidity('name', false);
+						return undefined;
+				}
+			});
+		  }
+	  };
+});
+
+nebulaModule.run(function($rootScope, $location,  $interpolate) {
+	$rootScope.$on('$routeChangeStart', function(event, next) {
+		var tu = next.$route.templateUrl;
+		if (tu) {
+			var otu = next.$route.originTemplateUrl;
+			if (!otu) {
+				otu = tu;
+				next.$route.originTemplateUrl = tu;
+			}
+			next.$route.templateUrl = $interpolate(otu)(next.params);
+		}
+	});
+});
