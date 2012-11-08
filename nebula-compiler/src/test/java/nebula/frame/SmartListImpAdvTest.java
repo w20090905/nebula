eb5dacbd096897611446776af8694c6dd65333d2
wangshilian <wangshilian@gmail.com>
2012-11-08 10:20:47 +0800

    add smartlist query test case

---
+++ nebula-compiler/src/test/java/nebula/frame/SmartListImpAdvTest.java
@@ -0,0 +1,77 @@
+package nebula.frame;
+
+import nebula.Filter;
+import nebula.Identifiable;
+import nebula.SmartList;
+import junit.framework.TestCase;
+
+public class SmartListImpAdvTest extends TestCase {
+	SmartListImp<Person> list = null;
+	String name = "test";
+
+	class Person {
+		String name;
+		int age;
+		int height;
+	}
+
+	Person p = null;
+
+	protected void setUp() throws Exception {
+		list = new SmartListImp<>(name, new Identifiable<Person>() {
+			@Override
+			public String getId(Person data) {
+				return data.name;
+			}
+		});
+		p = new Person();
+		p.name = "wangshilian";
+		p.age = 10;
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+
+	public final void testQuery() {
+		assertEquals(0, list.size());
+		list.add(p);
+		p = new Person();
+		p.name = "wang12";
+		p.age = 10;
+		list.add(p);
+		
+		p = new Person();
+		p.name = "wang13";
+		p.age = 12;
+		list.add(p);
+		
+		p = new Person();
+		p.name = "wang14";
+		p.age = 12;
+		list.add(p);
+		
+		p = new Person();
+		p.name = "wang15";
+		p.age = 13;
+		list.add(p);
+		
+		assertEquals(5, list.size());
+		SmartList<Person> result =  list.query(new Filter<SmartListImpAdvTest.Person>() {			
+			@Override
+			public boolean match(Person v) {
+				return v.age == 12;
+			}
+		});
+		assertEquals(2, result.size());
+		result.remove(1);
+		result.remove(0);
+		
+		assertEquals(0, result.size());
+		
+		assertEquals(5, list.size());
+		
+		
+	}
+}
