81f3d0e1e41687aeaf1090554f7b10abbe15a0a6
Byron.Wang <wangshilian@gmail.com>
2013-08-12 15:26:16 +0800

    change name

--- nebula-compiler/src/main/java/nebula/lang/OperatorExpr.java
+++ nebula-compiler/src/main/java/nebula/lang/OperatorExpr.java
@@ -3,7 +3,11 @@ package nebula.lang;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 
-public interface Operator {
+public interface OperatorExpr {
+
+	void arithmetic(final ClassWriter cw, final MethodVisitor mv, final Context context, final Operator op, final Expr<Object> e1, final Expr<Object> e2) ;
+	<V> void relational(ClassWriter cw, MethodVisitor mv, Context context, Operator op, Expr<V> e1, Expr<V> e2);
+	
 	<V> void eq(ClassWriter cw, final MethodVisitor mv,Context context, final Expr<V> e1, final Expr<V> e2);
 
 	<V> void ne(ClassWriter cw, final MethodVisitor mv,Context context, final Expr<V> e1, final Expr<V> e2);
