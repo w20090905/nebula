53b46bc3f3094417aea977c1f651f45f73432890
Byron.Wang <wangshilian@gmail.com>
2013-10-23 19:26:55 +0800

    refine adempiere name

--- nebula-engine/src/test/java/typeimport/DefaultImporter.java
+++ nebula-engine/src/test/java/typeimport/DefaultImporter.java
@@ -696,29 +696,31 @@ public class DefaultImporter {
 
 	static final Multiset<String> hadWords = TreeMultiset.create();
 	//@formatter:off
- static final String words = ("As Asp activity Accept Access Account Acct Achievement Acqusition Actual Address After Alert Allocation Allow Always Amt Amount Approve Approved Archive Archived Asset Attachment Attachments Attribute Auto " +
- 		"Balance bank Base Based basket batch Begin beginning benchmark Beta Between Bid binary black bom boundary bp Bpartner Bpcontact broadcast budget business Buyer " +
- 		"commodity current cumulative confidential Can Cash category Centrally Change Chare Charge chars chattype Class Classes Click Client collection color column columns Commission Committed Confirm Connection contact container Conversion copies copy Cost Costs costing Country Counter create created createdby credit Credit_card currency Customer cut_off " +
- 		"Due direct discontinued dashboard dyn Database Date Days Declaration Decimal Definite Delayed default Delivery Dependent Description Desktop Dimension Discount Display distribution Doc Document Download Dunning Duration " +
- 		"entry even exclude element Email Enforce End Ending entity Error Event expression Exp Expense export " +
- 		"func flat flow first Factory Fee Field File Files FileSystem Finish fix Footer Foreign Form Format Forecast Freight Frequency From Full Function Functionality Functions Function_symbols fund " +
- 		"gl group Grand Gross Guarantee Goal Grace " +
- 		"high has Hdr Header " +
- 		"increment Image Immediate Info Inactivity Inout instance Interest inventory Invoice Is Iso Is_Own Is_Online Is_Over Is_Overwrite Is_Owned Is_One Is_Orderby Issue Issues " +
- 		"Jsp join journal Jasper job " +
+ static final String words = ("" +
+ 		"AVS ARC A_P_A_R and_or ad_text adhoc add ACCUMULATION ALIGNMENT ACCUMDE ADVANCED ALL ALLOCATE ALLOCATED ANY APPROVAL AREA AUCTION AUTHOR AUTHORIZATION AVAILABLE AVERAGE As Asp activity Accept Access Account Acct Achievement Acqusition Actual Address After Alert Allocation Allow Always Amt Amount Approve Approved Archive Archived Asset Attachment Attachments Attribute Auto ALLOCATION Averaged ALLOCATION auth author authorization Addition Addr Address " +
+ 		"branch BAR Build Building Best By BACK BASIS BEFORE BELOW BILL BIRTHDAY BOOK BREAK Balance bank Base Based basket batch Begin beginning benchmark Beta Between Bid binary black bom boundary bp Bpartner Bp_contact broadcast budget business Buyer " +
+ 		"CANDIDATE C_BP CALENDAR COORDINATE COORDINATES CONVERTED COMPANY Chat cum CALCULATED CALCULATION CHARACTER CHECK CHEQUE CLOSE CLOSED CODE COL COLLAPSED COMMIT COMMITMENT COMPLETE CONFIG CONSTANT CONTENT CONTROL CORRECTS CORRECTION COUNT COUNTED C_STAGE CTL CUMULATED CUSTOM CUSTOMIZATION CUR CYCLE commodity current currency cumulative confidential Can Cash category Centrally Change Chare Charge chars Class Classes Click Client collection color column columns Commission Committed Confirm Connection contact container Conversion copies copy Cost Costs costing Country Counter create created created_by credit Credit_card currency Customer cut_off Configuration ConfirmEd Checked Close_date Cost_Standard currencies COMMITTED CONVERTED CHARGEABLE creator " +
+ 		"do_pricing disposal divide delta deviation DAY DAYS DATA DB DECISION DEMAND DEPRECIATED DEPRECIATION DETAIL DEVELOPER DIFFERENCE DECISION Due direct discontinued dashboard dyn Database Date Days Declaration Decimal Definite Delayed default Delivery Dependent Description Desktop Dimension Discount Display distribution Doc Document Download Dunning Duration DISTRIBUTION DECLARATION Details Differences Controlled DEPRECIATION drop_ship " +
+ 		"EE ELAPSED ED EDI EFT EMU ENCRYPTION ENCRYPTED entry even exclude element Email Enforce End Ending entity Error Event expression Exp Expense export entered exempt " +
+ 		"For Fail_on FILLED FINAL FUTURE func flat flow first Factory Fee Field File Files File_System Finish fix Footer Foreign Form Format Forecast FINANCIAL Freight Frequency From Full Function Functionality Functions Function_symbols fund Fixed fields " +
+ 		"GOOD GENERATE GENERATED GRAPH gl group Grand Gross Guarantee Goal Grace " +
+ 		"HOST HEIGHT high has Hdr Header " +
+ 		"imp import imported IF_CLEARING ID_RANGE INDUSTRY ITEM increment Image Immediate Info Inactivity Inout instance Interest inventory Invoice Interest Interests INVOICED " +
+ 		"INCOME INDEX INDEXED INSERT INTER INTERNAL INV Invoice INVITED Is Iso IS_POSITIVE Is_Own Is_Online Is_Over Is_Overwrite Is_Owned Is_One Is_Order_by IS_OFFER Issue Issues Interest Inventory INSTITUTION " +
+ 		"JAVA Jsp join journal Jasper job " +
  		"Key Keep " +
- 		"local lifetime Label Language Language_Iso Landed Last Ldap Level Levels Line Lines Line_Stroke Lingual List Logger lookup Lot " +
- 		"month months mail mandatory Margin match Max Method migration Model modify Movement Msg Multi " +
- 		"Name Natural New next No Non Not Node Note Notification number " +
- 		"Old On Online Only One Open Operation Or Order Orderby Ordered Org Orig Other Over Overwrite own Owned " +
- 		"publish priority Package Paint Page paper Path Pay Payer payment Per Percent Personal Pick Planned Po Point Port Pos Post postal P_instance Posting Position Preference Price Print Printed Printer Priority Procedure process Processor Processors Processing Processed Product Proj Project Proxy " +
- 		"Qa Qty Quantity Query Readonly " +
- 		"ratio repeat routing recognition Record Reference region registration related replenish release Remuneration replication report Request Require Required requisition Resource Response Responsible ression Revenue rfq_response Role " +
- 		"SWIFT self same Sales scheduler schema script seconds security Select Selection seller Send seq sequence sequentially ser_no Server service Set share shelf Ship Shipper Show Single Smtp So Sold Source Split Standard start Status statement Std Store sub subscription substitute Support Sync Sys System " +
- 		"tential track type table Target Task Tax Tender Template Test Text time Times topic Total Tracking transaction tree " +
- 		"under Unearned units Unix Unrealized Uom Use used User " +
- 		"valid validate Validation Value vendor Verify Version Voice " +
- 		"waiting warehouse Watch web webstore Week Weekday When Where win Window Windows workbench workflow Working Writeoff");
+ 		"LEAD LEASE LICIT LIFE LIMIT LOAD LOC LOCK LOCATOR LOCATION LOG logged Logon Login Lost local lifetime Label Language Language_Iso Landed Last Ldap Level Levels Line Lines Line_Stroke Lingual List Logger lookup Lot Logic Loader Lo_code List_Invoices " +
+ 		"MAINTENENCE MM MAINTENANCE MAINTAINED MANUAL MARK Market MEASURE MENU MESSAGE Min MINIMUM MODE month months mail mandatory Margin match Max Method migration Model modify Movement Msg Multi Maintenance Missing Moderator moderation Menu_Assets Menu_Payments Menu_Invoices MULTIPLIER " +
+ 		"NEGATIVE NET NULL Name Natural New next No Non Not Node nodes Note notesq Notification number " +
+ 		"OBSCURE OFFER OFFSET OPERATING Old On Online Only One Open Operation Or Order Order_by Ordered Org orgs Orig Other Over Overwrite own Owned OverDue Orders " +
+ 		"PHONE PRECIATION PRIVATE PROC PROD PROFILE PUBLIC PAID PARAM PARAMETER PARENTELEMENT PASTDUE PHASE PLATFORM PURCHASE PURCHASED publish priority Package Paint Page paper Path Pay Payer payment Per PERIOD Percent Personal Pick Planned Po Point Port Pos Post postal P_instance Posting Position Preference Price Print Printed Printer Priority Procedure process Processor Processors Processing Processed Product Proj Project Proxy PRODUCTION Parent Picked POTENTIAL Payee " +
+ 		"QUALITY QUOTE Qa Qty Quantity Query Readonly " +
+ 		"RECEIVE RECEIVED Recurring Relative Reval Revaluation RFQ Row RT Rule Run Running ratio repeat routing recognition Record Reference region registration related replenish release Remuneration replication report Request Require Required requisition Resource Response Responsible ression Revenue rfq_response Role runs remaining Reval_cr Reval_Dr Requests RfQs Registrations RESTRICTION RETAINED REALIZED " +
+ 		"Step Schedule Scrapped Scribe Scriber Share Sign Signment SQL Statistics Structure Suppress SWIFT self same Sales scheduler schema script seconds security Select Selected Selection seller Send seq sequence sequentially ser_no Server service Set share shelf Ship Shipper Show Single Smtp So Sold Source Split Standard start Status statement Std Store sub subscription substitute Support Sync Sys System Sort Subscribe SubScriber Shipments " +
+ 		"Term Termination Temporary Teardown Tandard Token Transition Transfer Trade Translation Translated tential track type table Target Task Trx Tax Tender Template Test Text time Times topic Total Tracking transaction tree Transferred " +
+ 		"Until under Unearned units Unix Unrealized Uom Use used User Update Updated Updates Unallocated Updateable UNEARNED " +
+ 		"valid validate Validation Value vendor Verify Version Voice vendors validator " +
+ 		"waiting warehouse Watch web web_store Week Weekday When Where Willing Willing_To win wiki Wstore Window Windows Work workbench workflow Working Write_off winner");
   //@formatter:on
 
 	static final String[] finds = words.toUpperCase().replace("_", "").split(" ");
