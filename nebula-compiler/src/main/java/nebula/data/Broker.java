5f1f90b7a14860fd72101e947ad49f8f5d269819
Byron.Wang <wangshilian@gmail.com>
2013-10-28 15:01:55 +0800

    remove old broker version

--- nebula-compiler/src/main/java/nebula/data/Broker.java
+++
@@ -1,7 +0,0 @@
-package nebula.data;
-
-public interface Broker<T> extends Timable {
-	void addWatcher(DataWatcher<T> listener);
-
-	T get();
-}
--- nebula-compiler/src/main/java/nebula/data/impl/BrokerEx.java
+++
@@ -1,7 +0,0 @@
-package nebula.data.impl;
-
-import nebula.data.Broker;
-
-interface BrokerEx<T> extends Broker<T> {
-	public void put(T newData) ;
-}
